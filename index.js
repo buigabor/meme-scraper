2065ee0ec997d89a5c4940a3cdcc34e18f1b1882
Bui Gábor <bui.gabor97@gmail.com>
2021-02-09 12:27:24 +0100

    Refactor code, can make memes with only bottom or top text

--- index.js
+++ index.js
@@ -2,71 +2,90 @@ const fs = require('fs');
 const path = require('path');
 const fetch = require('node-fetch');
 const cliProgress = require('cli-progress');
+const readline = require('readline');
 const { createMeme, listMemes } = require('./memeMaker');
 
 const url = 'https://memegen-link-examples-upleveled.netlify.app/';
 const bar1 = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
 
 function getImages(string) {
-	let match;
-	const urls = [];
-	const rex = /<img[^>]+src="([^">]+)"/g;
+  let match;
+  const urls = [];
+  const rex = /<img[^>]+src="([^">]+)"/g;
 
-	while ((match = rex.exec(string))) {
-		urls.push(match[1]);
-	}
+  while ((match = rex.exec(string))) {
+    urls.push(match[1]);
+  }
 
-	return urls;
+  return urls;
 }
 
 async function downloadImg(imgUrl, imgName) {
-	const pathToMemes = path.resolve(__dirname, 'memes', `${imgName}.png`);
+  const pathToMemes = path.resolve(__dirname, 'memes', `${imgName}.png`);
 
-	const response = await fetch(imgUrl);
-	const buffer = await response.buffer();
+  const response = await fetch(imgUrl);
+  const buffer = await response.buffer();
 
-	await fs.writeFile(pathToMemes, buffer, () => {
-		bar1.increment(10);
-	});
+  await fs.writeFile(pathToMemes, buffer, () => {
+    bar1.increment(10);
+  });
 }
 
 function generateName() {
-	return JSON.stringify(Math.random());
+  return JSON.stringify(Math.random());
 }
 
 async function scrapeMemes() {
-	const res = await fetch(url);
-	const body = await res.text();
+  const res = await fetch(url);
+  const body = await res.text();
 
-	const imgs = getImages(body);
+  const imgs = getImages(body);
 
-	const dir = './memes';
+  const dir = './memes';
 
-	if (!fs.existsSync(dir)) {
-		fs.mkdirSync(dir);
-	}
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
 
-	bar1.start(100, 0);
-	for (let i = 0; i < 10; i++) {
-		const imgName = generateName();
-		const imageUrl = imgs[i];
-		await downloadImg(imageUrl, imgName);
-	}
-	bar1.update(100);
-	bar1.stop();
+  bar1.start(100, 0);
+  for (let i = 0; i < 10; i++) {
+    const imgName = generateName();
+    const imageUrl = imgs[i];
+    await downloadImg(imageUrl, imgName);
+  }
+  bar1.update(100);
+  bar1.stop();
 }
 
 let topText = process.argv[2];
 let bottomText = process.argv[3];
 const memeName = process.argv[4];
 
-if (topText && bottomText && memeName) {
-	topText = topText.split('-').join(' ');
-	bottomText = bottomText.split('-').join(' ');
+if (process.argv[2] === '--help') {
+  listMemes();
+} else if (topText && bottomText && memeName) {
+  topText = topText.split('-').join(' ');
+  bottomText = bottomText.split('-').join(' ');
 
-	createMeme(topText, bottomText, memeName);
-} else if (process.argv[2] === '--help') {
-	listMemes();
+  createMeme(topText, bottomText, memeName);
+} else if (!process.argv[4]) {
+  const text = process.argv[2];
+  const memeKey = process.argv[3];
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question(
+    'Would like the text to be at the top or bottom?',
+    function (pos) {
+      if (pos === 'bottom') {
+        createMeme('', text, memeKey);
+        return rl.close();
+      }
+      createMeme(text, '', memeKey);
+      return rl.close();
+    },
+  );
 } else {
-	scrapeMemes();
+  scrapeMemes();
 }
--- memeMaker.js
+++ memeMaker.js
@@ -5,57 +5,53 @@ const memeCreator = require('meme-creator');
 const memeTemplateURL = 'https://api.memegen.link/templates';
 
 async function fetchMemeTemplate(url, memeName) {
-	const res = await fetch(url);
-	const body = await res.text();
-	const memeTempsArr = JSON.parse(body);
+  const res = await fetch(url);
+  const body = await res.text();
+  const memeTempsArr = JSON.parse(body);
 
-	const selectedMemeTemp = memeTempsArr.filter((memeTemp) => {
-		return memeTemp.key === memeName;
-	})[0];
+  if (!memeName) {
+    return memeTempsArr;
+  }
 
-	return selectedMemeTemp;
-}
+  const selectedMemeTemp = memeTempsArr.filter((memeTemp) => {
+    return memeTemp.id === memeName;
+  })[0];
 
-async function fetchMemeTemplates(url) {
-	const res = await fetch(url);
-	const body = await res.text();
-	const memeTempsArr = JSON.parse(body);
-
-	return memeTempsArr;
+  return selectedMemeTemp;
 }
 
 async function createMeme(topText, bottomText, memeName) {
-	// Make directory
-	const dir = './my-memes';
-	if (!fs.existsSync(dir)) {
-		fs.mkdirSync(dir);
-	}
+  // Make directory
+  const dir = './my-memes';
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
 
-	// Fetch specified memetemplate
-	const memeTemp = await fetchMemeTemplate(memeTemplateURL, memeName);
+  // Fetch specified meme template
+  const memeTemp = await fetchMemeTemplate(memeTemplateURL, memeName);
 
-	// Set meme parameters
-	const options = {
-		imageURL: `${memeTemp.blank}`, // URL to image
-		topText, // top text of meme
-		bottomText, // bottom text of meme
-		directory: './my-memes/', // where to save memes
-		fileName: `${memeTemp.name}`, // change to 'random' for a random file name
-	};
+  // Set meme parameters
+  const options = {
+    imageURL: `${memeTemp.blank}`, // URL to image
+    topText, // top text of meme
+    bottomText, // bottom text of meme
+    directory: './my-memes/', // where to save memes
+    fileName: `${memeTemp.name}`,
+  };
 
-	memeCreator(options, function (res, error) {
-		if (error) throw new Error(error);
+  memeCreator(options, function (res, error) {
+    if (error) throw new Error(error);
 
-		console.log('You can view your meme by going to ' + res.fileName);
-	});
+    console.log('You can view your meme by going to ' + res.fileName);
+  });
 }
 
 async function listMemes() {
-	const memesTemps = await fetchMemeTemplates(memeTemplateURL);
-	const memeSlugs = memesTemps.map((memeTemp) => {
-		return memeTemp.key;
-	});
-	console.dir(memeSlugs, { maxArrayLength: null });
+  const memesTemps = await fetchMemeTemplate(memeTemplateURL);
+  const memeSlugs = memesTemps.map((memeTemp) => {
+    return memeTemp.id;
+  });
+  console.dir(memeSlugs, { maxArrayLength: null });
 }
 
 module.exports = { createMeme, listMemes };
